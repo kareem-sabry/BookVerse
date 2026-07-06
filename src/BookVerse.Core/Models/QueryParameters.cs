5931c46c9b5b11f569d5c7a6a697d018921aa4be
kareem-sabry <kareemsabry.mail@gmail.com>
2026-07-06 19:14:26 +0300

    fix(pagination): clamp PageNumber and PageSize to a minimum of 1

--- src/BookVerse.Core/Models/QueryParameters.cs
+++ src/BookVerse.Core/Models/QueryParameters.cs
@@ -6,12 +6,18 @@ public class QueryParameters
 {
     private const int MaxPageSize = ApplicationConstants.MaxPageSize;
     private int _pageSize = ApplicationConstants.DefaultPageSize;
-    public int PageNumber { get; set; } = 1;
+    private int _pageNumber = 1;
+
+    public int PageNumber
+    {
+        get => _pageNumber;
+        set => _pageNumber = value < 1 ? 1 : value;
+    }
 
     public int PageSize
     {
         get => _pageSize;
-        set => _pageSize = value > MaxPageSize ? MaxPageSize : value;
+        set => _pageSize = value < 1 ? 1 : value > MaxPageSize ? MaxPageSize : value;
     }
 
     public string? SortBy { get; set; }
